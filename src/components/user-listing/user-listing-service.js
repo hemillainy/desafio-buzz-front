a36b9d791847a3f56a63dbabd808545f2bb7e837
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-10-14 01:49:43 -0300

    add checkboxs

--- src/components/user-listing/user-listing-service.js
+++ src/components/user-listing/user-listing-service.js
@@ -1,6 +1,9 @@
 angular.module("adminScreen")
     .factory("usersAPI", function ($http) {
-        const getUsers = function () {
-            return $http.get("http://localhost:3000/users")
+        const _getUsers = function () {
+            return $http.get("http://localhost:3000/users");
+        };
+        return {
+            getUsers: _getUsers
         }
     })
--- src/components/user-listing/user-listing.js
+++ src/components/user-listing/user-listing.js
@@ -2,6 +2,10 @@ angular.module("adminScreen")
     .component("userListing", {
         templateUrl: './src/components/user-listing/user-listing.html',
         controller: function ($scope, usersAPI) {
-            
+            usersAPI.getUsers()
+                .then(users => {
+                    console.log(users.data)
+                    $scope.users = users.data;
+                })
         }
     })
