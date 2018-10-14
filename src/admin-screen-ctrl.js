a48ed41b1de935266fb14f3a34508f39f020c5b5
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-10-14 02:59:36 -0300

    add ng-if em user-listing

--- src/admin-screen-ctrl.js
+++ src/admin-screen-ctrl.js
@@ -1,7 +1,12 @@
 angular.module("adminScreen").controller("AdminScreenCtrl", function ($scope) {
     $scope.viewSearch = false;
+    $scope.viewUserListing = false;
 
     $scope.setViewSearch = function () {
         $scope.viewSearch = !$scope.viewSearch;
+    }
+
+    $scope.setViewUserListing = function () {
+        $scope.viewUserListing = !$scope.viewUserListing;
     }
 })
