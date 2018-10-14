f2252bad204f8cfdf5db72e922c4401749c35f2f
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-10-14 01:20:16 -0300

    add user-listing

---
+++ src/components/user-listing/user-listing.js
@@ -0,0 +1,7 @@
+angular.module("adminScreen")
+    .component("userListing", {
+        templateUrl: './src/components/user-listing/user-listing.html',
+        controller: function ($scope, $http) {
+
+        }
+    })
